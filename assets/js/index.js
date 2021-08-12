effb0719e15e0d9157ca7028e964b72ebb8cbdb6
Rennan Melo <rennan.melo11@gmail.com>
2021-08-12 19:58:45 -0300

    Populate the select with all countries

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,9 +6,29 @@
 
   // Verify if the promises was fulfilled
   if (response[0].status == "fulfilled") {
-    console.log(await response[0].value.json());
+    loadCountries(await response[0].value.json());
   }
   if (response[1].status == "fulfilled") {
-    console.log(await response[1].value.json());
+    loadSummary(await response[1].value.json());
   }
 })();
+
+const loadCountries = (data) => {
+  let countries = document.getElementById("countries");
+
+  // Order the countries
+  data.sort((a, b) => {
+    let x = a.Country.toUpperCase();
+    let y = b.Country.toUpperCase();
+
+    return x === y ? 0 : x > y ? 1 : -1;
+  });
+
+  for (countryIndex in data) {
+    // Create an option html element to each country
+    countries.options[countries.options.length] = new Option(
+      data[countryIndex].Country,
+      data[countryIndex].Country
+    );
+  }
+};
